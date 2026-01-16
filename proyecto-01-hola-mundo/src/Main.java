afd695b6f23c8f77adecd96e3a214a554cb5de73
Alexis Arriola Martinez <20253ds138@utez.edu.mx>
2026-01-15 19:46:36 -0600

    Proyecto 01: hola mundo

---
+++ proyecto-01-hola-mundo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Alexis");
+        System.out.println("2E");
+        System.out.println("8/01/26");
+        System.out.println("Hola mundo");
+    }
+}
