125bef37f210ea3636b30017a65381846aa53040
Edgar Miguel Rivera Amaro <20253rd025@utez.edu.mx>
2026-02-05 09:35:22 -0600

    Control de califs v1

---
+++ ControlDeCalificaciones/src/App.java
@@ -0,0 +1,24 @@
+package src;
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+        GradeService gradeService = new GradeService();
+        Scanner sc = new Scanner(System.in);
+    }
+
+
+    public String leerTextoNoVacio(Scanner sc, String msg) {
+        return valor;
+    }
+    public double leerDoubleEnRango(Scanner sc, String msg, double min, double max){
+      return valor;
+    }
+    public int leerIntEnRango(Scanner sc, String msg, int min, int max){
+        return valor;
+    }
+    public boolean leerBoolean(Scanner sc, String msg){
+        return valor;
+    }
+}
+
---
+++ ControlDeCalificaciones/src/GradeService.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Scanner;
+
+public class GradeService {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    public double calcularPromedio(double a, double b, double c) {
+    return valor;
+    }
+    public double calcularFinal(double promedio, int asistencia){
+        return valor;
+    }
+    public String determinarEstado(double final, int asistencia, boolean entregoProyecto){
+        return valor;
+    }
+}
